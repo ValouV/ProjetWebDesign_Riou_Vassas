53c3c9d488bbdadbd74df30c976d307f8741e152
Valentin VASSAS <valentin.vassas@epfprojets-sceaux.com>
2018-03-14 15:46:22 +0100

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name projetWebDesignApp
+ * @description
+ * # projetWebDesignApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('projetWebDesignApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name projetWebDesignApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the projetWebDesignApp
+ */
+angular.module('projetWebDesignApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
