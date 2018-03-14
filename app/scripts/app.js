52a01b42d805dae31e48db5ed35056fae98c7ba3
Valentin VASSAS <valentin.vassas@epfprojets-sceaux.com>
2018-03-14 18:45:02 +0100

    country name from iso code to full name

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,7 +15,8 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'isoCountryFilter'
   ])
   .config(function ($routeProvider) {
     $routeProvider
