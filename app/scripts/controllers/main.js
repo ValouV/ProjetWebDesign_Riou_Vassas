54612f667603dd22beadfc06ea42c8eaae82001e
Valentin VASSAS <valentin.vassas@epfprojets-sceaux.com>
2018-03-15 12:15:42 +0100

    indentation main.js

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,27 +1,27 @@
 'use strict';
 
 /**
- * @ngdoc function
- * @name projetWebDesignApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the projetWebDesignApp
- */
+* @ngdoc function
+* @name projetWebDesignApp.controller:MainCtrl
+* @description
+* # MainCtrl
+* Controller of the projetWebDesignApp
+*/
 angular.module('projetWebDesignApp')
-  .controller('MainCtrl', function ($http) {
+.controller('MainCtrl', function ($http) {
 
-    var url = 'https://public.opendatasoft.com/api/records/1.0/search/?dataset=worldcitiespop&sort=population&facet=country';
+  var url = 'https://public.opendatasoft.com/api/records/1.0/search/?dataset=worldcitiespop&sort=population&facet=country';
 
-    //create map
-    mapboxgl.accessToken = 'pk.eyJ1IjoidmFsb3V2IiwiYSI6ImNqZXNhaGV5aTVkeTkycXBlMGV3bGNycXYifQ.F57k7NEQSKGD3Gc_rAunCQ';
-    var map = new mapboxgl.Map({
+  //create map
+  mapboxgl.accessToken = 'pk.eyJ1IjoidmFsb3V2IiwiYSI6ImNqZXNhaGV5aTVkeTkycXBlMGV3bGNycXYifQ.F57k7NEQSKGD3Gc_rAunCQ';
+  var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/satellite-v9'
-    });
+  });
 
-    //when map load
-    map.on('load', function() {
-      //get json from url
+  //when map load
+  map.on('load', function() {
+    //get json from url
     $http.jsonp(url).then(function(data) {
       //point layout creation
       var geoPoints = {
@@ -29,14 +29,14 @@ angular.module('projetWebDesignApp')
         'source': {
           'type': 'geojson',
           'data': {}
-          },
+        },
         'paint': {
-            'circle-radius': {
-                'base': 1.75,
-                'stops': [[1, 20], [10, 180]]
-            },
-            'circle-color': '#fbb03b'
-          }
+          'circle-radius': {
+            'base': 1.75,
+            'stops': [[1, 20], [10, 180]]
+          },
+          'circle-color': '#fbb03b'
+        }
       };
       //then for each point in json
       angular.forEach(data.data.records, function(value, key){
@@ -44,11 +44,11 @@ angular.module('projetWebDesignApp')
         geoPoints.source.data = value.geometry;
         geoPoints.id = value.recordid,
         map.addLayer(
-            geoPoints
+          geoPoints
         );
       });
 
     });
-    });
-
   });
+
+});
