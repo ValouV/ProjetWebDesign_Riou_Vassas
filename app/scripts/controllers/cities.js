c658661bb90985449ef59984d643379a791cbaa6
Valentin VASSAS <valentin.vassas@epfprojets-sceaux.com>
2018-03-16 16:54:01 +0100

    fix url tab

--- app/scripts/controllers/cities.js
+++ app/scripts/controllers/cities.js
@@ -9,17 +9,15 @@
  */
 angular.module('projetWebDesignApp')
   .controller('CitiesCtrl', function ($scope, $http) {
-    var url = 'https://public.opendatasoft.com/api/records/1.0/search/?dataset=worldcitiespop&sort=population&facet=country';
-
 
       $scope.currentPage = 0;
       $scope.pageSize = 20;
 
-    var url = 'https://public.opendatasoft.com/api/records/1.0/search/?dataset=worldcitiespop&rows=10&start=&sort=population&facet=country';
+    var url = 'https://public.opendatasoft.com/api/records/1.0/search/?dataset=worldcitiespop&rows=10&start=&sort=population';
 
 
       $scope.$watch('currentPage + pageSize', function(){
-        url = 'https://public.opendatasoft.com/api/records/1.0/search/?dataset=worldcitiespop&rows='+$scope.pageSize+'&start='+$scope.currentPage+'&sort=population&facet=country';
+        url = 'https://public.opendatasoft.com/api/records/1.0/search/?dataset=worldcitiespop&rows='+$scope.pageSize+'&start='+$scope.currentPage+'&sort=population';
             $http.jsonp(url).
       then(function(data) {
         $scope.list = data.data.records;
