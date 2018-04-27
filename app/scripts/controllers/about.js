3eedd3206b202ea969ca631ecc83cc910c144afa
Valentin VASSAS <valentin.vassas@epfprojets-sceaux.com>
2018-04-27 19:50:08 +0200

    modif about

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -8,13 +8,6 @@
  * Controller of the projetWebDesignApp
  */
 angular.module('projetWebDesignApp')
-  .controller('AboutCtrl', function ($scope) {
+  .controller('AboutCtrl', function () {
 
-      $scope.imagePath = 'images/angular.png';
-
-  }).config(function($mdThemingProvider) {
-    $mdThemingProvider.theme('dark-grey').backgroundPalette('grey').dark();
-    $mdThemingProvider.theme('dark-orange').backgroundPalette('orange').dark();
-    $mdThemingProvider.theme('dark-purple').backgroundPalette('deep-purple').dark();
-    $mdThemingProvider.theme('dark-blue').backgroundPalette('blue').dark();
   });
