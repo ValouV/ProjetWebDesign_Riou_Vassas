bdd780f2b39ce816bf4f78f78199f08c14a3015c
Louis_R <lr.louisriou@gmail.com>
2018-04-28 16:29:42 +0200

    Fin about, color card, comments, readme

--- README.md
+++ README.md
@@ -1,8 +1,23 @@
 # projet-web-design
 
-This project is generated with [yo angular generator](https://github.com/yeoman/generator-angular)
-version 0.16.0.
+Instruction pour ouvrir l'application WEB :
+-Télécharger le dossier
+-Ouvrir un terminal (powershell recommandé sur windows)
+-Aller à la racine du dossier : taper : cd chemin_ou_est_le_dossier
+-Installer NPM : taper : npm install
+-Installer Bower : taper : npm install -g bower
+-Installer les dépendances via bower : taper : bower install
+-Lancer l'application en local en tapant : grunt serve
 
-## Build & development
+Fonctionnalités implémentées :
+-AngularJS utilisé, code indenté et commenté
+-Utilisation d'un webservice externe pour avoir des données
+-Utilisation du format JSON pour ces données
+-Utilisation des directives core
+-Site web personnalisé et unique 
+-Site web respectant les principes de material design
+-Site web responsive
+-Filtre custom pour afficher 1M6 plutot que 1600000 dans la population des villes
 
-Run `grunt` for building and `grunt serve` for preview.
+Fonctionnalités supplémentaires :
+-Utilisation de mapboxgl afin d'avoir une carte zoomable et d'y afficher des points sur les villes les plus peuplées ces points étant plus ou moins gros selon la popoluation de la ville
--- app/scripts/controllers/cities.js
+++ app/scripts/controllers/cities.js
@@ -9,25 +9,25 @@
  */
 angular.module('projetWebDesignApp')
   .controller('CitiesCtrl', function ($scope, $http) {
-
+//Variable to interact with the url of the API
       $scope.currentPage = 0;
       $scope.pageSize = 20;
       $scope.ordrecroissant=true;
+//URL to obtain data from the API
+    var url;
 
-    var url = 'https://public.opendatasoft.com/api/records/1.0/search/?dataset=worldcitiespop&rows=10&start=&sort=population';
 
-
-      $scope.$watch('currentPage + pageSize + ordrecroissant', function(){
+      $scope.$watch('currentPage + pageSize + ordrecroissant', function(){ //put value in the URL to have the right data
         if ($scope.ordrecroissant) {url = 'https://public.opendatasoft.com/api/records/1.0/search/?dataset=worldcitiespop&rows='+$scope.pageSize+'&start='+$scope.currentPage+'&sort=population'};
         if (!$scope.ordrecroissant) {url = 'https://public.opendatasoft.com/api/records/1.0/search/?dataset=worldcitiespop&rows='+$scope.pageSize+'&start='+$scope.currentPage+'&sort=-population'};  
-            $http.jsonp(url).
+            $http.jsonp(url). //get the data as a json file
       then(function(data) {
-        $scope.list = data.data.records;
+        $scope.list = data.data.records; //put the data in list
       });
 
       });
 
-  }).filter('milions', function() {
+  }).filter('milions', function() {//allows with a custom filter to have 1M instead of 1 000 000 when population is higher than a million
 
     return function(input) {
       if (input!==undefined){
